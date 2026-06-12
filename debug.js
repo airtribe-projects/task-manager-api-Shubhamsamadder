dfa4739b6fae20eddb789785220402c0b3260215
Shubham Samadder <shubhamsamadder16@gmail.com>
2026-06-12 20:56:07 +0530

    Implementation of Task Manager API

---
+++ debug.js
@@ -0,0 +1,3 @@
+console.log("1");
+const app = require("./app");
+console.log("2");
---
+++ routes/taskRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllTasks,
+  getTaskById,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/taskController");
+
+router.get("/", getAllTasks);
+router.get("/:id", getTaskById);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
+
+router.get("/priority/:level",getTasksByPriority);
+
+module.exports = router;
